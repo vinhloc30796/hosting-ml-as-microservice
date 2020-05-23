454792146c7f0b60f3b60d761db89366ecaa42a6
Loc Nguyen <nguyenloc300796@gmail.com>
2020-05-23 13:56:08 +0700

    Move sentiment prediction functions to separate .py file

---
+++ part3/pred.py
@@ -0,0 +1,51 @@
+# Import packages
+from string import punctuation
+from nltk.tokenize import word_tokenize
+from nltk import download
+from nltk.corpus import stopwords
+import pickle
+import sys
+# Download datasets
+download('punkt')
+download('stopwords')
+# Set stopwords
+stopwords_eng = stopwords.words('english')
+# Import model
+if not 'google.colab' in sys.modules:
+    model_file = open('sa_classifier.pickle', 'rb')
+    model = pickle.load(model_file)
+    model_file.close()
+# Define functions
+def extract_features(words):
+    """
+    Remove stopwords from words
+    Input:
+    - words (list of string)
+    Output
+    - list of string
+    """
+    return [w for w in words if w not in stopwords_eng and w not in punctuation]
+def bag_of_words(words):
+    """
+    Tally word occurences into a dict
+    Input:
+    - words (list of strings)
+    Output:
+    - bad (dict of (string: int) pairs)
+    """
+    bag = {}
+    for w in words:
+        bag[w] = bag.get(w,0)+1
+    return bag
+def get_sentiment(review):
+    """
+    Return 'pos' or 'neg' sentiment for the review
+    Input:
+    - review (str)
+    Output:
+    - str: 'pos' for positive and 'neg' for negative
+    """
+    words = word_tokenize(review)
+    words = extract_features(words)
+    words = bag_of_words(words)
+    return model.classify(words)
